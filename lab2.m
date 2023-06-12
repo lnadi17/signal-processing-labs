e67ca633a61acfb602df2b7c27cf5b37c5ca6e6e
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2023-06-13 03:33:36 +0400

    Add IIR filter part to lab2

--- lab2.m
+++ lab2.m
@@ -60,3 +60,46 @@ sound(y);
 fvtool(tf2sos([1 -2*cos(2*pi*440/Fs) 1], [1 0 0]));
 
 %%
+
+r1 = 0.99; r2 = 0.9; r3 = 0.7;
+w = -pi:0.01:pi;
+H = @(r) (1 - r) ./ (1 - 2*r*cos(pi/3)*exp(-1j*w) + r^2 * exp(-2j*w));
+
+figure
+subplot(3, 1, 1);
+plot(w, abs(H(r1)));
+title('r = 0.99');
+
+subplot(3, 1, 2);
+plot(w, abs(H(r2)));
+title('r = 0.9');
+
+subplot(3, 1, 3);
+plot(w, abs(H(r3)));
+title('r = 0.7');
+
+%%
+
+load('pcm.mat');
+x = pcm; Fs = 8000;
+sound(x, Fs);
+
+%%
+
+[X, w] = DTFT(x(100:200), 1024);
+plot(w, abs(X));
+
+%%
+
+theta = 2*pi*3146/Fs;
+r = 0.995;
+[X, w] = DTFT(x, 0);
+H = (1 - r) ./ (1 - 2*r*cos(theta)*exp(-1j*w) + r^2 * exp(-2j*w));
+Y = X .* H;
+plot(w, abs(Y));
+xlim([theta-0.02 theta+0.02])
+
+%%
+
+x = 20*real(ifft(Y)); % Had to multiply by 20 hear the sound clearly
+sound(x, Fs);
