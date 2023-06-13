948ea012c477059c816a12ead27d8a047763426c
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2023-06-13 18:42:58 +0400

    Add lab3 first part

---
+++ lab3.m
@@ -0,0 +1,35 @@
+r = roots([1 -1 1]);
+disp(r);
+% Use these roots in pezdemo
+
+%%
+
+r = roots([1 1 1 1 1 1]);
+disp(r);
+% Use these roots in pezdemo
+
+%%
+
+r = roots([1 0 0 0 0 0 -1]);
+disp(r);
+% Use these roots in pezdemo
+
+%%
+
+r = roots([1 0 -1]);
+p = roots([1 0.8 0.64]);
+disp(r);
+disp(p);
+% Use r as poles and p as poles in pezdemo
+
+%%
+
+r = roots([64 80 100]);
+p = roots([1 0.8 0.64]);
+disp(r);
+disp(p);
+% Use r as poles and p as poles in pezdemo
+
+%%
+
+% 
